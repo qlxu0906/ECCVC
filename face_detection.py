f7b40cca34c157cc39c7aadbdbf3eee5c363c8b3
liliangqi <Lewis_lee@sjtu.edu.cn>
2018-05-30 22:35:10 +0800

    Add function `assign_id_to_face`

--- face_detection.py
+++ face_detection.py
@@ -11,6 +11,8 @@ import random
 import face_recognition
 import matplotlib.pyplot as plt
 import argparse
+import numpy as np
+import pandas as pd
 
 from __init__ import clock_non_return
 import sfd_demo
@@ -68,6 +70,43 @@ def show_detection_example(root_dir, tool='face_rec'):
     plt.tight_layout()
     plt.show()
     plt.close(fig)
+
+
+def assign_id_to_face(im_path, g_df, tool):
+    """
+    Detect faces and assign them with IDs from person
+    ---
+    param:
+        im_path: absolute path of the image to be detected
+        g_df: DataFrame that only contain information about a single image
+        tool: type of detector
+    return:
+
+    """
+
+    if tool == 'face_rec':
+        img = face_recognition.load_image_file(im_path)
+        face_locations = face_recognition.face_locations(
+            img, number_of_times_to_upsample=0, model='cnn')
+    elif tool == 'sfd':
+        face_locations = sfd_demo.demo(im_path)
+    else:
+        raise KeyError(tool)
+
+    faces = []
+    for face_loc in face_locations:
+        # Print the location of each face
+        if tool == 'face_rec':
+            y1, x2, y2, x1 = face_loc
+        elif tool == 'sfd':
+            x1, y1, x2, y2 = face_loc
+        else:
+            raise KeyError(tool)
+
+        face_box = np.array([x1, y1, x2 - x1, y2 - y1])
+        for i in range(g_df.shape[0]):
+            person_box = g_df.ix[i, 'x1': 'del_y'].as_matrix()
+            person_id = g_df.ix[i, 'pid']
 
 
 @clock_non_return
